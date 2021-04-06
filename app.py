40843e15f9e361eecd789a9a38408f14c8784e50
Div Loop <80190976+thedivloop@users.noreply.github.com>
2021-04-06 18:55:17 +0700

    added new logs table

--- app.py
+++ app.py
@@ -5,127 +5,168 @@ import model
 app = Flask(__name__)
 app.secret_key = 'jumpjacks'
 
+administrator = ''
 username = ''
 user = model.check_users()
 
-@app.route('/', methods = ['GET'])
+
+@app.route('/', methods=['GET'])
 def home():
-	if 'username' in session:
-		g.user = session['username']
-		return render_template('dashboard.html', message = model.get_lists(g.user) )
-		#message = '<table><tr><td>First Row</td></tr><tr><tr><td>First Row</td></tr><tr><tr><td>First Row</td></tr></table>')
-		# add the lists to show here "message ="
-	return render_template('homepage.html', message = "Log in to the page or sign up!")
+    if 'username' in session:
+        g.user = session['username']
+        return render_template('dashboard.html', message=model.get_lists(g.user))
+        # message = '<table><tr><td>First Row</td></tr><tr><tr><td>First Row</td></tr><tr><tr><td>First Row</td></tr></table>')
+        # add the lists to show here "message ="
+    return render_template('homepage.html', message="Log in to the page or sign up!")
 
-@app.route('/terms', methods = ['GET'])
+
+@app.route('/terms', methods=['GET'])
 def terms():
-	status = "out"
-	if 'username' in session:
-		status = "in"
-	return render_template('terms.html', message = status)
+    status = "out"
+    if 'username' in session:
+        status = "in"
+    return render_template('terms.html', message=status)
 
-@app.route('/privacy', methods = ['GET'])
+
+@app.route('/privacy', methods=['GET'])
 def privacy():
-	status = "out"
-	if 'username' in session:
-		status = "in"
-	return render_template('privacy.html', message = status)
+    status = "out"
+    if 'username' in session:
+        status = "in"
+    return render_template('privacy.html', message=status)
 
-@app.route('/about', methods = ['GET'])
+
+@app.route('/about', methods=['GET'])
 def about():
-	status = "out"
-	if 'username' in session:
-		status = "in"
-	return render_template('about.html', message = status)
+    status = "out"
+    if 'username' in session:
+        status = "in"
+    return render_template('about.html', message=status)
 
-@app.route('/dashboard', methods = ['GET'])
+
+@app.route('/dashboard', methods=['GET'])
 def dashboard():
-	return render_template('dashboard.html')
+    return render_template('dashboard.html')
 
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		session.pop('username', None)
-		areyouuser = request.form['username']
-		pwd = model.check_pw(areyouuser)
-		if request.form['password'] == pwd:
-			session['username'] = request.form['username']
-			return redirect(url_for('home'))
-	return render_template('index.html')
+    if request.method == 'POST':
+        session.pop('username', None)
+        areyouuser = request.form['username']
+        pwd = model.check_pw(areyouuser)
+        if request.form['password'] == pwd:
+            session['username'] = request.form['username']
+            return redirect(url_for('home'))
+    return render_template('index.html')
+
 
 @app.before_request
 def before_request():
-	g.username = None
-	if username in session:
-		g.username = session['username']
+    g.username = None
+    if username in session:
+        g.username = session['username']
 
 
-@app.route('/signup', methods = ['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-	if request.method == 'GET':
-		message = 'Please sign up!'
-		return render_template('signup.html')
-	else:
-		username = request.form["username"]
-		firstname = request.form["firstname"]
-		lastname = request.form["lastname"]
-		password = request.form["password"]
-		message = model.signup(username,firstname, lastname, password)
-		return redirect(url_for('login'))
+    if request.method == 'GET':
+        message = 'Please sign up!'
+        return render_template('signup.html')
+    else:
+        username = request.form["username"]
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+        password = request.form["password"]
+        message = model.signup(username, firstname, lastname, password)
+        return redirect(url_for('login'))
+
 
 @app.route('/getsession')
 def getsession():
-	if 'username' in session:
-		return session['username']
-	return redirect(url_for('login'))
+    if 'username' in session:
+        return session['username']
+    return redirect(url_for('login'))
 
-@app.route('/createlist', methods=['GET','POST'])
+
+@app.route('/createlist', methods=['GET', 'POST'])
 def createlist():
-	if request.method == 'POST':
-		list = request.form['list']
-		g.user = session['username']
-		userID = model.getUserId(g.user)
-		model.add_list(userID,list)
-		return redirect(url_for('home'))
-	else:
-		return render_template('createlist.html')
+    if request.method == 'POST':
+        list = request.form['list']
+        g.user = session['username']
+        userID = model.getUserId(g.user)
+        model.add_list(userID, list)
+        return redirect(url_for('home'))
+    else:
+        return render_template('createlist.html')
 
-@app.route('/<int:tid>/delete',methods=['GET','POST'])
+
+@app.route('/<int:tid>/delete', methods=['GET', 'POST'])
 def deletetask(tid):
-	model.delete_task(tid)
-	# g.user = session['username']
-	return redirect(url_for('home'))
+    model.delete_task(tid)
+    # g.user = session['username']
+    return redirect(url_for('home'))
 
-@app.route('/<int:lid>/deletelist',methods=['GET','POST'])
+
+@app.route('/<int:lid>/deletelist', methods=['GET', 'POST'])
 def deletelist(lid):
-	model.delete_list(lid)
-	# g.user = session['username']
-	return redirect(url_for('home'))
+    model.delete_list(lid)
+    # g.user = session['username']
+    return redirect(url_for('home'))
 
-@app.route('/<int:lid>/add',methods=['GET','POST'])
+
+@app.route('/<int:lid>/add', methods=['GET', 'POST'])
 def createtask(lid):
-	model.add_task(lid,'new task')
-	# g.user = session['username']
-	return redirect(url_for('home'))
+    model.add_task(lid, 'new task')
+    # g.user = session['username']
+    return redirect(url_for('home'))
 
-@app.route('/<int:tid>/updatetask', methods=['GET','POST'])
+
+@app.route('/<int:tid>/updatetask', methods=['GET', 'POST'])
 def update_task(tid):
-	if request.method == 'POST':
-		task = request.form['task']
-		g.user = session['username']
-		userID = model.getUserId(g.user)
-		model.update_task(tid,task)
-		return redirect(url_for('home'))
-	else:
-		tasktext='this is the text'
-		print(model.get_tasktext(tid))
-		return render_template('modifytask.html', text = model.get_tasktext(tid))
+    if request.method == 'POST':
+        task = request.form['task']
+        g.user = session['username']
+        userID = model.getUserId(g.user)
+        model.update_task(tid, task)
+        return redirect(url_for('home'))
+    else:
+        tasktext = 'this is the text'
+        print(model.get_tasktext(tid))
+        return render_template('modifytask.html', text=model.get_tasktext(tid))
+
 
 @app.route('/logout')
 def logout():
-	session.pop('username', None)
-	return redirect(url_for('home'))
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'GET':
+        return render_template('admin.html')
+    else:
+        session.pop('admin', None)
+        areyouadmin = request.form['admin']
+        pwd = model.check_pw_admin(areyouadmin)
+        if request.form['password'] == pwd:
+            session['admin'] = request.form['admin']
+            return redirect(url_for('admin_dash'))
+
+
+@app.route('/admin-dashboard', methods=['GET', 'POST'])
+def admin_dash():
+    if 'admin' in session:
+        return render_template('admin-dashboard.html', message='')
+    return redirect(url_for('admin_login'))
+
+
+@app.route('/admin-logout')
+def logout_admin():
+    session.pop('admin', None)
+    return redirect(url_for('admin_login'))
+
 
 if __name__ == '__main__':
-	app.run(port = 7000, debug = True)
+    app.run(port=7000, debug=True)
--- model.py
+++ model.py
@@ -4,212 +4,275 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 # get the list of the lists for a specified userID
+
+
 def get_lists(username):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	cursor.execute(""" SELECT userID FROM users WHERE username = '{username}' ;""".format(username= username))
-	userIdent = cursor.fetchone()[0]
-	print(userIdent)
-	cursor.execute(""" SELECT listID, listname FROM lists WHERE userID = '{userID}'; """.format(userID = userIdent))
-	db_lists = cursor.fetchall()
-	print(db_lists)
-	lists = []
-	tasks = []
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(""" SELECT userID FROM users WHERE username = '{username}' ;""".format(
+        username=username))
+    userIdent = cursor.fetchone()[0]
+    print(userIdent)
+    cursor.execute(""" SELECT listID, listname FROM lists WHERE userID = '{userID}'; """.format(
+        userID=userIdent))
+    db_lists = cursor.fetchall()
+    print(db_lists)
+    lists = []
+    tasks = []
 
-	for i in range(len(db_lists)):
-		row = db_lists[i][0]
-		lists.append(row)
+    for i in range(len(db_lists)):
+        row = db_lists[i][0]
+        lists.append(row)
 
-	for l in lists:
-		cursor.execute(""" SELECT listID, taskID,taskname FROM tasks WHERE listID = '{listID}'; """.format(listID = l))
-		db_tasks = cursor.fetchall()
-		for t in db_tasks:
-			tasks.append(t)
-		print(db_tasks)
+    for l in lists:
+        cursor.execute(
+            """ SELECT listID, taskID,taskname FROM tasks WHERE listID = '{listID}'; """.format(listID=l))
+        db_tasks = cursor.fetchall()
+        for t in db_tasks:
+            tasks.append(t)
+        print(db_tasks)
 
+    connection.commit()
+    cursor.close()
+    connection.close()
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-	
-	return db_lists, tasks
+    return db_lists, tasks
 
 
 # get the list of the tasks for a specified listID
 def get_tasks(list):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	cursor.execute(""" SELECT tasktname FROM tasks WHERE listID = '{list}' ORDER BY listID DESC; """)
-	db_tasks = cursor.fetchall()
-	tasks = []
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(
+        """ SELECT tasktname FROM tasks WHERE listID = '{list}' ORDER BY listID DESC; """)
+    db_tasks = cursor.fetchall()
+    tasks = []
 
-	for i in range(len(db_tasks)):
-		row = db_tasks[i][0]
-		tasks.append(row)
+    for i in range(len(db_tasks)):
+        row = db_tasks[i][0]
+        tasks.append(row)
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-	
-	return tasks
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return tasks
+
 
 def get_tasktext(tid):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	cursor.execute(""" SELECT taskname FROM tasks WHERE taskID = '{tid}';""".format(tid = tid))
-	tasktext = cursor.fetchone()[0]
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(
+        """ SELECT taskname FROM tasks WHERE taskID = '{tid}';""".format(tid=tid))
+    tasktext = cursor.fetchone()[0]
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-	
-	return tasktext
+    connection.commit()
+    cursor.close()
+    connection.close()
 
-# create a new list for a specified userID	
-def add_list(userID,listname):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	numberoftasks = 0
-	cursor.execute(""" INSERT INTO lists(userID, listname, numberoftasks) VALUES ('{userID}', '{listname}', '{numberoftasks}')""".format(userID = userID, listname = listname, numberoftasks = 0))
+    return tasktext
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-	print(listname,"successfully added to user", userID)
-	return listname + " successfully added to user " + str(userID)
+# create a new list for a specified userID
+
+
+def add_list(userID, listname):
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    numberoftasks = 0
+    cursor.execute(""" INSERT INTO lists(userID, listname, numberoftasks) VALUES ('{userID}', '{listname}', '{numberoftasks}')""".format(
+        userID=userID, listname=listname, numberoftasks=0))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print(listname, "successfully added to user", userID)
+    return listname + " successfully added to user " + str(userID)
 
 # create a new task for a specified listID
-def add_task(listID,taskname):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	numberoftasks = 0
-	cursor.execute(""" INSERT INTO tasks(listID, taskname) VALUES ('{listID}', '{taskname}')""".format(listID = listID, taskname = taskname))
 
-	cursor.execute(""" SELECT numberoftasks FROM lists WHERE listID = '{listID}' ORDER BY listID DESC; """.format(listID = listID))
-	newnumberoftasks = cursor.fetchone()[0] + 1
 
-	#cursor.execute(""" UPDATE lists SET numberoftasks = 2 WHERE listID = '{listID}'""".format(listID = listID))
-	cursor.execute(""" UPDATE lists SET numberoftasks = '{newnumberoftasks}' WHERE listID = '{listID}'""".format(newnumberoftasks = newnumberoftasks, listID = listID))
+def add_task(listID, taskname):
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    numberoftasks = 0
+    cursor.execute(""" INSERT INTO tasks(listID, taskname) VALUES ('{listID}', '{taskname}')""".format(
+        listID=listID, taskname=taskname))
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-	
-	return print(taskname,"successfully added to list", listID)
+    cursor.execute(
+        """ SELECT numberoftasks FROM lists WHERE listID = '{listID}' ORDER BY listID DESC; """.format(listID=listID))
+    newnumberoftasks = cursor.fetchone()[0] + 1
+
+    # cursor.execute(""" UPDATE lists SET numberoftasks = 2 WHERE listID = '{listID}'""".format(listID = listID))
+    cursor.execute(""" UPDATE lists SET numberoftasks = '{newnumberoftasks}' WHERE listID = '{listID}'""".format(
+        newnumberoftasks=newnumberoftasks, listID=listID))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return print(taskname, "successfully added to list", listID)
 
 # modify a task
-def update_task(taskID,newtaskname):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	cursor.execute(""" UPDATE tasks SET taskname = '{newtaskname}' WHERE taskID = '{taskID}'""".format(newtaskname = newtaskname, taskID = taskID))
 
-	connection.commit()
-	cursor.close()
-	connection.close()
 
-	return print("Task ID",taskID,"successfully update to ", newtaskname)
+def update_task(taskID, newtaskname):
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(""" UPDATE tasks SET taskname = '{newtaskname}' WHERE taskID = '{taskID}'""".format(
+        newtaskname=newtaskname, taskID=taskID))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return print("Task ID", taskID, "successfully update to ", newtaskname)
 
 # deletes a task based on the taskID
+
+
 def delete_task(taskID):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	numberoftasks = 0
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    numberoftasks = 0
 
-	cursor.execute(""" SELECT listID FROM tasks WHERE taskID = '{taskID}' ORDER BY listID DESC; """.format(taskID = taskID))
-	listID = cursor.fetchone()[0]
+    cursor.execute(
+        """ SELECT listID FROM tasks WHERE taskID = '{taskID}' ORDER BY listID DESC; """.format(taskID=taskID))
+    listID = cursor.fetchone()[0]
 
-	cursor.execute(""" DELETE FROM tasks WHERE taskID = '{taskID}'""".format(taskID = taskID))
+    cursor.execute(
+        """ DELETE FROM tasks WHERE taskID = '{taskID}'""".format(taskID=taskID))
 
-	cursor.execute(""" SELECT numberoftasks FROM lists WHERE listID = '{listID}' ORDER BY listID DESC; """.format(listID = listID))
-	newnumberoftasks = max(cursor.fetchone()[0] - 1,0)
+    cursor.execute(
+        """ SELECT numberoftasks FROM lists WHERE listID = '{listID}' ORDER BY listID DESC; """.format(listID=listID))
+    newnumberoftasks = max(cursor.fetchone()[0] - 1, 0)
 
-	#cursor.execute(""" UPDATE lists SET numberoftasks = 2 WHERE listID = '{listID}'""".format(listID = listID))
-	cursor.execute(""" UPDATE lists SET numberoftasks = '{newnumberoftasks}' WHERE listID = '{listID}'""".format(newnumberoftasks = newnumberoftasks, listID = listID))
+    # cursor.execute(""" UPDATE lists SET numberoftasks = 2 WHERE listID = '{listID}'""".format(listID = listID))
+    cursor.execute(""" UPDATE lists SET numberoftasks = '{newnumberoftasks}' WHERE listID = '{listID}'""".format(
+        newnumberoftasks=newnumberoftasks, listID=listID))
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-	
-	return print(taskID,"successfully deleted")
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return print(taskID, "successfully deleted")
 # modify a list
-def update_list(listID,newlistname):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	cursor.execute(""" UPDATE lists SET listname = '{newlistname}' WHERE listID = '{listID}'""".format(newlistname = newlistname, listID = listID))
 
-	connection.commit()
-	cursor.close()
-	connection.close()
 
-	return print("List ID",listID,"successfully update to ", newlistname)
+def update_list(listID, newlistname):
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(""" UPDATE lists SET listname = '{newlistname}' WHERE listID = '{listID}'""".format(
+        newlistname=newlistname, listID=listID))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return print("List ID", listID, "successfully update to ", newlistname)
 
 # deletes a list based on the listID
-def delete_list(list):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	numberoftasks = 0
-	cursor.execute(""" DELETE FROM lists WHERE listID = '{list}'""".format(list = list))
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-	
-	return print(list,"successfully deleted")
-	
+
+def delete_list(list):
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    numberoftasks = 0
+    cursor.execute(
+        """ DELETE FROM lists WHERE listID = '{list}'""".format(list=list))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return print(list, "successfully deleted")
+
 
 def check_pw(username):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	cursor.execute(""" SELECT password FROM users WHERE username = '{username}' ORDER BY userID DESC; """.format(username = username))
-	password = cursor.fetchone()[0]
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(""" SELECT password FROM users WHERE username = '{username}' ORDER BY userID DESC; """.format(
+        username=username))
+    password = cursor.fetchone()[0]
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-	
-	return password
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return password
+
+
+def check_pw_admin(username):
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(""" SELECT password FROM admin WHERE username = '{username}' ORDER BY username DESC; """.format(
+        username=username))
+
+    password = cursor.fetchone()[0]
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return password
+
 
 def signup(username, firstname, lastname, password):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	cursor.execute(""" SELECT password FROM users WHERE username = '{username}' ORDER BY userID DESC; """.format(username = username))
-	exist = cursor.fetchone()
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(""" SELECT password FROM users WHERE username = '{username}' ORDER BY userID DESC; """.format(
+        username=username))
+    exist = cursor.fetchone()
 
-	if exist is None:
-		cursor.execute(""" INSERT INTO users(username, firstname, lastname, password)VALUES('{username}', '{firstname}', '{lastname}','{password}')""".format(username = username, firstname = firstname, lastname = lastname,password = password))
-		connection.commit()
-		cursor.close()
-		connection.close()
-	else:
-		return('User already existed!!')
+    if exist is None:
+        cursor.execute(""" INSERT INTO users(username, firstname, lastname, password)VALUES('{username}', '{firstname}', '{lastname}','{password}')""".format(
+            username=username, firstname=firstname, lastname=lastname, password=password))
+        connection.commit()
+        cursor.close()
+        connection.close()
+    else:
+        return('User already existed!!')
 
-	return 'You have successfully signed up!'
+    return 'You have successfully signed up!'
+
 
 def check_users():
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	cursor.execute(""" SELECT username FROM users ORDER BY userID DESC; """)
-	db_users = cursor.fetchall()
-	users = []
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(""" SELECT username FROM users ORDER BY userID DESC; """)
+    db_users = cursor.fetchall()
+    users = []
 
-	for i in range(len(db_users)):
-		person = db_users[i][0]
-		users.append(person)
+    for i in range(len(db_users)):
+        person = db_users[i][0]
+        users.append(person)
 
-	connection.commit()
-	cursor.close()
-	connection.close()
-	
-	return users
-	
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return users
+
+
 def getUserId(username):
-	connection = sqlite3.connect('project.db', check_same_thread = False)
-	cursor = connection.cursor()
-	cursor.execute(""" SELECT userID FROM users WHERE username='{username}'; """.format(username=username))
-	db_users = cursor.fetchall()
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute(""" SELECT userID FROM users WHERE username='{username}'; """.format(
+        username=username))
+    db_users = cursor.fetchall()
 
-	connection.commit()
-	cursor.close()
-	connection.close()
+    connection.commit()
+    cursor.close()
+    connection.close()
 
-	return db_users[0][0]
+    return db_users[0][0]
+
+
+def setLog(userId):
+    connection = sqlite3.connect('project.db', check_same_thread=False)
+    cursor = connection.cursor()
+    numberoftasks = 0
+    cursor.execute(
+        """ INSERT INTO logs(userId) VALUES ('{userId}')""".format(userId=userId))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
